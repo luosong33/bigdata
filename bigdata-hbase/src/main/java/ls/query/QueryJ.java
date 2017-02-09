e54102e0bff3462cb49cfad629106b0548c74c7a
luosong <luosong3345@gmail.com>
2017-02-09 10:51:45 +0800

    02091051

---
+++ bigdata-hbase/src/main/java/ls/query/QueryJ.java
@@ -0,0 +1,10 @@
+package ls.query;
+
+/**
+ * Created by Administrator on 2016/12/27.
+ */
+public class QueryJ {
+
+
+
+}
---
+++ bigdata-hbase/src/main/java/ls/util/HbaseConn.java
@@ -0,0 +1,39 @@
+package ls.util;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2016/12/30.
+ */
+public class HbaseConn {
+
+    static Configuration config = null;
+    static {
+        config = HBaseConfiguration.create();
+        config.set("hbase.zookeeper.quorum", "node1,node2,node3");
+        config.set("hbase.zookeeper.property.clientPort", "2181");
+    }
+
+    Connection conn = null;
+    {
+        try {
+            conn = ConnectionFactory.createConnection(config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getHbaseConn(){
+        return new HbaseConn().conn;
+    }
+
+    public static Configuration getHbaseConf(){
+        return config;
+    }
+
+}
